702b8ecca731d2adfaa8746bd2eb6e894046c693
Dejan Bozhinoski <bozhinoski@icloud.com>
2024-10-26 22:41:52 +0200

    02: alterative viewChild approach; viewChildren

--- 02-starting-project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 02-starting-project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,11 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  viewChild,
+  ViewChild,
+  viewChildren,
+  ViewChildren,
+} from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
 import { FormsModule } from '@angular/forms';
@@ -11,10 +18,17 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
-  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  // @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  // private form = viewChild<ElementRef<HTMLFormElement>>('form');
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+  // @ViewChildren('input') private inputs?: ElementRef<HTMLInputElement>[];
+  // private inputs = viewChildren<HTMLInputElement | HTMLTextAreaElement>(
+  //   'input'
+  // );
 
   onSubmit(title: string, request: string) {
     console.log('Submitted', title, request);
-    this.form?.nativeElement.reset();
+    this.form().nativeElement.reset();
   }
 }
