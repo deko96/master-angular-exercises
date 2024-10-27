1fde2587844af6a55b7fa70342f382a284f34aef
Dejan Bozhinoski <bozhinoski@icloud.com>
2024-10-27 21:12:15 +0100

    03: implement onConfirmLeave handler in SafeLinkDirective

--- 03-starting-project/src/app/safe-link.directive.ts
+++ 03-starting-project/src/app/safe-link.directive.ts
@@ -3,9 +3,21 @@ import { Directive, Injectable } from '@angular/core';
 @Directive({
   selector: '[appSafeLink]',
   standalone: true,
+  host: {
+    '(click)': 'onConfirmLeavePage($event)',
+  },
 })
 export class SafeLinkDirective {
   constructor() {
     console.log('SafeLinkDirective is active!');
+  }
+
+  onConfirmLeavePage(event: MouseEvent) {
+    const wantsToLeave = window.confirm('Do you want to leave the app?');
+    if (wantsToLeave) {
+      return;
+    }
+
+    event.preventDefault();
   }
 }
